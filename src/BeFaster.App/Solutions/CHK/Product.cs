a2642e7819bfb88af2814509117252fc23e97329
sbx_user1051 <sbx_user1051@169.254.29.19>
2025-07-19 15:22:09 +0000

    Sat Jul 19 15:22:09 UTC 2025

--- src/BeFaster.App/Solutions/CHK/Product.cs
+++ src/BeFaster.App/Solutions/CHK/Product.cs
@@ -1,10 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BeFaster.App.Solutions.CHK {
+﻿namespace BeFaster.App.Solutions.CHK {
     internal class Product {
+        public char SKU { get; }
+        public int Price { get; }
+
+        public List<(int Amount, int Price)> Offers { get; } = new List<(int, int)>();
+        public (int amount, int freeAmount, char freeSKU)? FreeOffer { get; set; };
+
+        public Product(char sku, int price) {
+            SKU = sku;
+            Price = price;
+        }
     }
 }
+
