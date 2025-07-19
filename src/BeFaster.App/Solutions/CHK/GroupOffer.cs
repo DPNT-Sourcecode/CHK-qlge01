176ee146af7fcbf82a0f0c0df59cbbd1b4bed4e9
sbx_user1051 <sbx_user1051@169.254.29.19>
2025-07-19 15:52:33 +0000

    Sat Jul 19 15:52:33 UTC 2025

--- src/BeFaster.App/Solutions/CHK/GroupOffer.cs
+++ src/BeFaster.App/Solutions/CHK/GroupOffer.cs
@@ -1,10 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BeFaster.App.Solutions.CHK {
+﻿namespace BeFaster.App.Solutions.CHK {
     internal class GroupOffer {
+        public HashSet<char> SKUs { get; }
+        public int RequiredAmount { get; }
+        public int Price { get; }
+
+        public GroupOffer(IEnumerable<char> skus, int requiredAmount, int price) {
+            SKUs = new HashSet<char>(skus);
+            RequiredAmount = requiredAmount;
+            Price = price;
+        }
     }
 }
+
