9307aa1f29340386ce1b667fd6dcc398b80378db
sbx_user1051 <sbx_user1051@169.254.29.19>
2025-07-19 15:49:33 +0000

    Sat Jul 19 15:49:33 UTC 2025

---
+++ src/BeFaster.App/Solutions/CHK/GroupOffer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeFaster.App.Solutions.CHK {
+    internal class GroupOffer {
+    }
+}
