e2fdf34e68c11e343818b1f56d1c4923495d3690
sbx_user1051 <sbx_user1051@169.254.29.19>
2025-07-19 15:42:08 +0000

    Sat Jul 19 15:42:08 UTC 2025

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -1,5 +1,37 @@
 ﻿namespace BeFaster.App.Solutions.CHK {
     public class CheckoutSolution {
+        static Product FreeOfferHelper(Product product, int amount, int freeAmount, char freeSKU) {
+            product.FreeOffer = (amount, freeAmount, freeSKU);
+            return product;
+        }
+        Dictionary<char, Product> products = new Dictionary<char, Product> {
+            ['A'] = new Product('A', 50) { Offers = { (5, 200), (3, 130) } },
+            ['B'] = new Product('B', 30) { Offers = { (2, 45) } },
+            ['C'] = new Product('C', 20),
+            ['D'] = new Product('D', 15),
+            ['E'] = FreeOfferHelper(new Product('E', 40), 2, 1, 'B'),
+            ['F'] = FreeOfferHelper(new Product('F', 10), 2, 1, 'F'),
+            ['G'] = new Product('G', 20),
+            ['H'] = new Product('H', 10) { Offers = { (10, 80), (5, 45) } },
+            ['I'] = new Product('I', 35),
+            ['J'] = new Product('J', 60),
+            ['K'] = new Product('K', 80) { Offers = { (2, 150) } },
+            ['L'] = new Product('L', 90),
+            ['M'] = new Product('M', 15),
+            ['N'] = FreeOfferHelper(new Product('N', 40), 3, 1, 'M'),
+            ['O'] = new Product('O', 10),
+            ['P'] = new Product('P', 50) { Offers = { (5, 200) } },
+            ['Q'] = new Product('Q', 30) { Offers = { (3, 80) } },
+            ['R'] = FreeOfferHelper(new Product('R', 50), 3, 1, 'Q'),
+            ['S'] = new Product('S', 30),
+            ['T'] = new Product('T', 20),
+            ['U'] = FreeOfferHelper(new Product('U', 40), 3, 1, 'U'),
+            ['V'] = new Product('V', 50) { Offers = { (3, 130), (2, 90) } },
+            ['W'] = new Product('W', 20),
+            ['X'] = new Product('X', 90),
+            ['Y'] = new Product('Y', 10),
+            ['Z'] = new Product('Z', 50),
+        };
         public int Checkout(string? skus) {
             Dictionary<char, int> prices = new Dictionary<char, int> {
                 {'A', 50},
@@ -20,42 +52,6 @@
                 {'F', (2, 1, 'F')}
             };
 
-            Product FreeOfferHelper(Product product, int amount, int freeAmount, char freeSKU) {
-                product.FreeOffer = (amount, freeAmount, freeSKU);
-                return product;
-            }
-
-
-            Dictionary<char, Product> products = new Dictionary<char, Product> {
-                ['A'] = new Product('A', 50) { Offers = { (5, 200), (3, 130) } },
-                ['B'] = new Product('B', 30) { Offers = { (2, 45) } },
-                ['C'] = new Product('C', 20),
-                ['D'] = new Product('D', 15),
-                ['E'] = FreeOfferHelper(new Product('E', 40), 2, 1, 'B'),
-                ['F'] = FreeOfferHelper(new Product('F', 10), 2, 1, 'F'),
-                ['G'] = new Product('G', 20),
-                ['H'] = new Product('H', 10) { Offers = { (10, 80), (5, 45) } },
-                ['I'] = new Product('I', 35),
-                ['J'] = new Product('J', 60),
-                ['K'] = new Product('K', 80) { Offers = { (2, 150) } },
-                ['L'] = new Product('L', 90),
-                ['M'] = new Product('M', 15),
-                ['N'] = FreeOfferHelper(new Product('N', 40), 3, 1, 'M'),
-                ['O'] = new Product('O', 10),
-                ['P'] = new Product('P', 50) { Offers = { (5, 200) } },
-                ['Q'] = new Product('Q', 30) { Offers = { (3, 80) } },
-                ['R'] = FreeOfferHelper(new Product('R', 50), 3, 1, 'Q'),
-                ['S'] = new Product('S', 30),
-                ['T'] = new Product('T', 20),
-                ['U'] = FreeOfferHelper(new Product('U', 40), 3, 1, 'U'),
-                ['V'] = new Product('V', 50) { Offers = { (3, 130), (2, 90) } },
-                ['W'] = new Product('W', 20),
-                ['X'] = new Product('X', 90),
-                ['Y'] = new Product('Y', 10),
-                ['Z'] = new Product('Z', 50),
-            };
-
-
             Dictionary<char, int> shopping = new Dictionary<char, int>();
 
             int total = 0;
@@ -73,13 +69,18 @@
                 shopping[item]++;
             }
 
-            foreach (var (promoItem, (amount, freeAmount, freeItem)) in freeOffers) {
-                if (shopping.TryGetValue(promoItem, out int promoCount)) {
-                    if (promoItem == freeItem) {
+            foreach (var (sku, product) in products) {
+                if (!product.FreeOffer.HasValue) {
+                    continue;
+                }
+                var (amount, freeAmount, freeItem) = product.FreeOffer.Value;
+
+                if (shopping.TryGetValue(sku, out int promoCount)) {
+                    if (sku == freeItem) {
                         int size = amount + freeAmount;
                         int totalFree = (promoCount / size) * freeAmount;
 
-                        shopping[promoItem] = Math.Max(0, promoCount - totalFree);
+                        shopping[sku] = Math.Max(0, promoCount - totalFree);
                     } else {
                         if (shopping.TryGetValue(freeItem, out int freeCount)) {
                             int totalFree = (promoCount / amount) * freeAmount;
@@ -101,7 +102,6 @@
                         total += offerQuantity * offer.price;
 
                         amount %= offer.amount;
-
                     }
                 }
 
@@ -112,3 +112,4 @@
         }
     }
 }
+
