923cf488a99cc7161ae3b45fc3e6fc407ab0b2de
sbx_user1051 <sbx_user1051@169.254.9.137>
2025-07-19 13:25:58 +0000

    Sat Jul 19 13:25:58 UTC 2025

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -1,12 +1,19 @@
-﻿using BeFaster.Runner.Exceptions;
+﻿namespace BeFaster.App.Solutions.CHK {
+    public class CheckoutSolution {
+        public int Checkout(string? skus) {
+            Dictionary<char, int> prices = new Dictionary<char, int> {
+                {'A', 50},
+                {'B', 30},
+                {'C', 20},
+                {'D', 15}
+            };
 
-namespace BeFaster.App.Solutions.CHK
-{
-    public class CheckoutSolution
-    {
-        public int Checkout(string? skus)
-        {
-            throw new SolutionNotImplementedException();
+            Dictionary<char, (int amount, int price)> offers = new Dictionary<char, (int amount, int price)> {
+                {'A', (3, 130)},
+                {'B', (2, 45)}
+            };
+            return -1;
         }
     }
 }
+
